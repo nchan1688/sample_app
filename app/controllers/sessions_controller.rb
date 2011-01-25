d0e71e82a8a31f35e6c651db9d1ada6d97b100f6
Nathan Chan <npchan@cal.berkeley.edu>
2011-01-24 21:34:37 -0800

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      #Handle successful signin.
+      sign_in user
+      redirect_to user
     end
     
   end
